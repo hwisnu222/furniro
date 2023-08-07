a0c52f12e5e16d9db211e4f2fb42a76632185229
Wisnu Box <hwisnu222@gmail.com>
2023-08-07 09:45:50 +0700

    fix: delete help menu footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,15 +6,15 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <Container className="tw-mt-6">
-      <div className="tw-mb-4 tw-grid tw-grid-cols-1 tw-gap-4 tw-text-center md:tw-grid-cols-4 md:tw-text-left">
+      <div className="tw-mb-4 tw-grid tw-grid-cols-1 tw-gap-8 tw-text-center md:tw-grid-cols-3 md:tw-text-left">
         <div>
-          <h4 className="tw-mb-8 tw-text-xl tw-font-bold">Funiro.</h4>
+          <h4 className="tw-text-xl tw-font-bold md:tw-mb-8">Funiro.</h4>
           <p className="tw-text-gray-300">
             400 University Drive Suite 200 Coral Gables, FL 33134 USA
           </p>
         </div>
         <div>
-          <h4 className="tw-mb-8 tw-font-bold tw-text-gray-300">Links</h4>
+          <h4 className="tw-font-bold tw-text-gray-300 md:tw-mb-8">Links</h4>
           <ul className="tw-flex tw-flex-col tw-gap-8 tw-font-bold">
             <li>
               <Link href="/" className="hover:tw-text-gray-600">
@@ -39,15 +39,9 @@ export default function Footer() {
           </ul>
         </div>
         <div>
-          <h4 className="tw-mb-8 tw-font-bold tw-text-gray-300">Help</h4>
-          <ul className="tw-flex tw-flex-col tw-gap-8 tw-font-bold">
-            <li>Payment Options</li>
-            <li>Returns</li>
-            <li>Privacy Policies</li>
-          </ul>
-        </div>
-        <div>
-          <h4 className="tw-mb-8 tw-font-bold tw-text-gray-300">Newsletter</h4>
+          <h4 className="tw-font-bold tw-text-gray-300 md:tw-mb-8">
+            Newsletter
+          </h4>
           <div className="tw-flex tw-flex-col md:tw-flex-row">
             <Input
               className="tw-mr-2 tw-text-gray-400"
