275ee8d270148abeb875400d2e1aa98385c525ce
Wisnu Box <hwisnu222@gmail.com>
2023-08-15 13:54:35 +0700

    fix: add link to auth layout logo

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import { Box } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 import Logo from "@/assets/images/furniro-large.svg";
 
@@ -8,12 +9,14 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <Box className="tw-flex">
       <Box className="tw-hidden tw-h-screen tw-w-full tw-items-center tw-justify-center tw-border-r tw-border-default-200 tw-bg-default-100 md:tw-flex">
-        <Image
-          src={Logo}
-          alt="logo"
-          fill={false}
-          className="tw-h-16 tw-w-auto"
-        />
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="logo"
+            fill={false}
+            className="tw-h-16 tw-w-auto"
+          />
+        </Link>
       </Box>
       <Box className="tw-flex tw-h-screen tw-w-full tw-flex-col tw-items-center tw-justify-center">
         {children}
