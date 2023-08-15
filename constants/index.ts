31bd299766d3ab6a5e17aaa687c12568dcde6a61
Wisnu Box <hwisnu222@gmail.com>
2023-08-15 11:24:58 +0700

    fix: constant variable

---
+++ constants/index.ts
@@ -0,0 +1,10 @@
+export enum STATUS_AUTH {
+  authenticated = "authenticated",
+  unauthenticated = "unauthenticated",
+}
+
+export enum ROLE {
+  Authenticated = "Authenticated",
+  Public = "Public",
+  Admin = "Admin",
+}
