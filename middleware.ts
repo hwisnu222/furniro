70efcd3f2a291cd49308ac94f5881a4c6057d194
Wisnu Box <hwisnu222@gmail.com>
2023-08-15 12:07:40 +0700

    feat: add authorization middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,34 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import { ROLE } from "./constants";
+import { signOut } from "next-auth/react";
 
 export async function middleware(req: NextRequest) {
   const session = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
   });
-  console.log(session);
 
-  if (session) {
+  const pathname = req.nextUrl.pathname;
+  // if url in path user
+  if (pathname.startsWith("/user") && session?.role === ROLE.Authenticated) {
     return NextResponse.next();
   }
-  //   return NextResponse.next();
+
+  // if url in path admin
+  if (pathname.startsWith("/admin") && session?.role === ROLE.Admin) {
+    return NextResponse.next();
+  }
+
+  // if pathname does't match and have session redirect user to logout
+  // or forbidden
+  if (session) {
+    signOut({
+      callbackUrl: "/auth/login",
+    });
+  }
+
+  //  if user not have session redirect to login
   return NextResponse.redirect(new URL("/auth/login", req.url));
 }
 
--- nextauth.d.ts
+++ nextauth.d.ts
@@ -2,10 +2,12 @@ import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
-    user: {
-      role: string | number;
-      token: string;
-    };
+    // user: {
+    //   role?: string | number;
+    //   token?: string;
+    // };
+    role?: string;
+    token?: string;
   }
   interface JWT {
     role: string | number;
