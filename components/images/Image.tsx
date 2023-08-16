51aff31ddb71e43e071c1ec51dd861259bca721f
Wisnu Box <hwisnu222@gmail.com>
2023-08-16 08:36:52 +0700

    fix: add skeleton on image

--- components/images/Image.tsx
+++ components/images/Image.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ImageNext from "next/image";
+import { Skeleton } from "@mui/material";
 
 export default function Image(props: any) {
+  const [loading, setLoading] = React.useState(true);
   return (
-    <ImageNext
-      alt="thumbnail"
-      width={200}
-      height={400}
-      {...props}
-      src={props.src && process.env.NEXT_PUBLIC_MEDIA + props.src}
-    />
+    <>
+      {loading && (
+        <Skeleton variant="rectangular" className="tw-h-full tw-w-full" />
+      )}
+      <ImageNext
+        hidden={loading}
+        alt="thumbnail"
+        width={200}
+        height={400}
+        {...props}
+        src={props.src && process.env.NEXT_PUBLIC_MEDIA + props.src}
+        onLoad={() => {
+          setLoading(false);
+        }}
+      />
+    </>
   );
 }
