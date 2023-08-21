60430be29bb24ae5eddf517ba856f013e35ede66
Wisnu Box <hwisnu222@gmail.com>
2023-08-21 13:26:09 +0700

    feat: authorization user

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,7 +1,26 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { getSession } from "next-auth/react";
+
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL,
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const session = await getSession();
+  const token = session?.user.token;
+  console.log(token);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL,
+  link: authLink.concat(httpLink),
+  // uri: process.env.NEXT_PUBLIC_GRAPHQL,
   cache: new InMemoryCache(),
 });
 
