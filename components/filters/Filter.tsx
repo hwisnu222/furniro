7c1b060f689350a6172bb2fd4a029a33a0e03154
Wisnu Box <hwisnu222@gmail.com>
2023-08-22 09:38:59 +0700

    fix: reset filter product

--- components/filters/Filter.tsx
+++ components/filters/Filter.tsx
@@ -91,7 +91,6 @@ export default function Filter({ onApply }: { onApply: Function }) {
     setHighPrice("");
     setLowPrice("");
     setCategory("");
-    applyFilter();
   };
   return (
     <>
