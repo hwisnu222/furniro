7dbf606cc88b8b74f5d61837b5b7e456cd568c1b
Wisnu Box <hwisnu222@gmail.com>
2023-08-24 09:27:52 +0700

    fix: add wishlist feedback

--- components/cards/Products.tsx
+++ components/cards/Products.tsx
@@ -24,12 +24,16 @@ import {
 import { GET_WISHLISTS } from "@/graphql/queries/wishlist.query";
 import { WishListItem } from "@/interfaces/wishlist.interface";
 
-export default function Products(props: {
-  data: ItemProduct[];
+const ProductCard = ({
+  item,
+  refetch,
+}: {
+  item: ItemProduct;
   refetch?: any;
-}) {
+}) => {
   const session = useSession();
   const [wishlists, setWishlist] = React.useState([]);
+  const [check, setCheck] = React.useState(false);
 
   const [addCart] = useMutation(ADD_CART);
   const [addWishlist] = useMutation(CREATE_WISHLIST);
@@ -48,25 +52,31 @@ export default function Products(props: {
     },
   });
 
-  const checkWishlist = (id: number) => {
-    const listId = wishlists.filter(
-      (item: WishListItem) => item.attributes.product.data?.id === id,
-    );
-    return listId[0]?.["id"];
-  };
+  const checkWishlist = React.useCallback(
+    (id: number) => {
+      const listId = wishlists.filter(
+        (item: WishListItem) => item.attributes.product.data?.id === id,
+      );
+      console.log(listId);
+      return listId[0]?.["id"];
+    },
+    [wishlists],
+  );
 
-  const handleAddWishlist = (id: number) => {
+  const handleAddWishlist = () => {
     const idUser = session.data?.user.id;
     addWishlist({
       variables: {
         data: {
-          product: id,
+          product: item.id,
           users_permissions_user: idUser,
         },
       },
       onCompleted: () => {
         refetchWishlist();
-        props.refetch();
+        refetch();
+
+        setCheck(true);
         enqueueSnackbar("Product has added to wishlist", {
           variant: "success",
         });
@@ -77,14 +87,16 @@ export default function Products(props: {
     });
   };
 
-  const handleDeleteWishlist = (id: number) => {
+  const handleDeleteWishlist = () => {
+    const idWishlist = checkWishlist(item.id);
     deleteWishlist({
       variables: {
-        id,
+        id: idWishlist,
       },
       onCompleted: () => {
         refetchWishlist();
-        props.refetch();
+        refetch();
+        setCheck(false);
         enqueueSnackbar("Wishlist is deleted!", { variant: "success" });
       },
       onError: () => {
@@ -93,12 +105,12 @@ export default function Products(props: {
     });
   };
 
-  const handleAddCart = (id: number) => {
+  const handleAddCart = () => {
     addCart({
       variables: {
         data: {
           total: 1,
-          product: id,
+          product: item.id,
           users_permissions_user: session.data?.user.id,
         },
       },
@@ -110,89 +122,181 @@ export default function Products(props: {
       },
     });
   };
+
+  React.useEffect(() => {
+    if (checkWishlist(item.id)) {
+      setCheck(true);
+    }
+  }, [checkWishlist, item.id]);
+  return (
+    <Box className="tw-relative tw-gap-4 tw-rounded-md tw-bg-gray-200 tw-text-left">
+      <div className="tw-absolute tw-left-0 tw-top-0 tw-z-20 tw-flex tw-h-full tw-w-full tw-flex-col tw-items-center tw-justify-center tw-bg-[#00000080] tw-opacity-0 tw-duration-300 hover:tw-h-full hover:tw-opacity-100">
+        <Button
+          onClick={() => handleAddCart()}
+          variant="contained"
+          className="tw-mb-8 tw-bg-white tw-text-default-200 hover:tw-text-white"
+        >
+          Add to cart
+        </Button>
+        <ul className="tw-grid tw-grid-cols-2 tw-gap-4 tw-font-bold tw-text-white">
+          <li className="cursor-pointer tw-flex tw-items-center tw-gap-1 tw-text-sm">
+            <Reply className="tw-h-6 tw-w-6" /> Share
+          </li>
+          <li className="cursor-pointer tw-flex tw-cursor-pointer tw-items-center tw-gap-1 tw-text-sm">
+            {check ? (
+              <IconButton onClick={() => handleDeleteWishlist()}>
+                <Favorite className="tw-h-6 tw-w-6 tw-text-red-400" />
+              </IconButton>
+            ) : (
+              <IconButton onClick={() => handleAddWishlist()}>
+                <FavoriteBorder className="tw-h-6 tw-w-6 tw-text-white" />
+              </IconButton>
+            )}
+
+            <span>Like</span>
+          </li>
+          <Link href={`/product-comparison?current=${item.attributes.slug}`}>
+            <li className="cursor-pointer tw-flex tw-items-center tw-gap-1 tw-text-sm">
+              <Compare className="tw-h-6 tw-w-6" />
+              Compare
+            </li>
+          </Link>
+          <Link href={`/single-product/${item.attributes.slug}`}>
+            <li className="cursor-pointer  tw-flex tw-items-center tw-gap-1 tw-text-sm">
+              <Details />
+              Detail
+            </li>
+          </Link>
+        </ul>
+      </div>
+
+      <Avatar className="tw-absolute tw-right-4 tw-top-4 tw-bg-red-400">
+        <span className="tw-text-sm">{item?.attributes?.disscount}%</span>
+      </Avatar>
+
+      <Image
+        src={
+          process.env.NEXT_PUBLIC_MEDIA +
+          item.attributes.image?.data[0].attributes.url
+        }
+        alt="products"
+        fill={false}
+        width={200}
+        height={200}
+        className="tw-h-72 tw-w-full tw-object-cover"
+      />
+      <Box className="tw-flex tw-flex-col tw-gap-2 tw-p-4">
+        <h3 className="tw-text-xl tw-font-bold">{item.attributes.name}</h3>
+        <p className="tw-text-gray-500">
+          {item.attributes.category?.data?.attributes.category || "-"}
+        </p>
+        <div className="tw-flex tw-flex-col tw-items-start tw-justify-between tw-gap-2 md:tw-flex-row md:tw-items-center">
+          <s className="tw-inline tw-text-sm tw-text-gray-400">
+            {convertCurrency(item?.attributes.price)}
+          </s>
+          <strong className="tw-inline tw-text-default-200">
+            {convertCurrency(
+              (item?.attributes.disscount / 100) * item?.attributes.price,
+            )}
+          </strong>
+        </div>
+      </Box>
+    </Box>
+  );
+};
+
+export default function Products(props: {
+  data: ItemProduct[];
+  refetch?: any;
+}) {
   return (
     <div className="tw-mb-8 tw-grid tw-grid-cols-1 tw-gap-4 md:tw-grid-cols-4">
       {props.data?.map((item: ItemProduct, index: number) => (
-        <Box
+        <ProductCard
+          item={item}
+          refetch={props.refetch}
           key={`product-${index}`}
-          className="tw-relative tw-gap-4 tw-rounded-md tw-bg-gray-200 tw-text-left"
-        >
-          <div className="tw-absolute tw-left-0 tw-top-0 tw-z-20 tw-flex tw-h-full tw-w-full tw-flex-col tw-items-center tw-justify-center tw-bg-[#00000080] tw-opacity-0 tw-duration-300 hover:tw-h-full hover:tw-opacity-100">
-            <Button
-              onClick={() => handleAddCart(item.id)}
-              variant="contained"
-              className="tw-mb-8 tw-bg-white tw-text-default-200 hover:tw-text-white"
-            >
-              Add to cart
-            </Button>
-            <ul className="tw-grid tw-grid-cols-2 tw-gap-4 tw-font-bold tw-text-white">
-              <li className="cursor-pointer tw-flex tw-items-center tw-gap-1 tw-text-sm">
-                <Reply className="tw-h-6 tw-w-6" /> Share
-              </li>
-              <li className="cursor-pointer tw-flex tw-cursor-pointer tw-items-center tw-gap-1 tw-text-sm">
-                {checkWishlist(item.id) ? (
-                  <IconButton
-                    onClick={() => handleDeleteWishlist(checkWishlist(item.id))}
-                  >
-                    <Favorite className="tw-h-6 tw-w-6 tw-text-red-400" />
-                  </IconButton>
-                ) : (
-                  <IconButton onClick={() => handleAddWishlist(item.id)}>
-                    <FavoriteBorder className="tw-h-6 tw-w-6 tw-text-white" />
-                  </IconButton>
-                )}
+        />
+        // <Box
+        //   key={`product-${index}`}
+        //   className="tw-relative tw-gap-4 tw-rounded-md tw-bg-gray-200 tw-text-left"
+        // >
+        //   <div className="tw-absolute tw-left-0 tw-top-0 tw-z-20 tw-flex tw-h-full tw-w-full tw-flex-col tw-items-center tw-justify-center tw-bg-[#00000080] tw-opacity-0 tw-duration-300 hover:tw-h-full hover:tw-opacity-100">
+        //     <Button
+        //       onClick={() => handleAddCart(item.id)}
+        //       variant="contained"
+        //       className="tw-mb-8 tw-bg-white tw-text-default-200 hover:tw-text-white"
+        //     >
+        //       Add to cart
+        //     </Button>
+        //     <ul className="tw-grid tw-grid-cols-2 tw-gap-4 tw-font-bold tw-text-white">
+        //       <li className="cursor-pointer tw-flex tw-items-center tw-gap-1 tw-text-sm">
+        //         <Reply className="tw-h-6 tw-w-6" /> Share
+        //       </li>
+        //       <li className="cursor-pointer tw-flex tw-cursor-pointer tw-items-center tw-gap-1 tw-text-sm">
+        //         {check ? (
+        //           <IconButton
+        //             onClick={() => handleDeleteWishlist(checkWishlist(item.id))}
+        //           >
+        //             <Favorite className="tw-h-6 tw-w-6 tw-text-red-400" />
+        //           </IconButton>
+        //         ) : (
+        //           <IconButton onClick={() => handleAddWishlist(item.id)}>
+        //             <FavoriteBorder className="tw-h-6 tw-w-6 tw-text-white" />
+        //           </IconButton>
+        //         )}
 
-                <span>Like</span>
-              </li>
-              <Link
-                href={`/product-comparison?current=${item.attributes.slug}`}
-              >
-                <li className="cursor-pointer tw-flex tw-items-center tw-gap-1 tw-text-sm">
-                  <Compare className="tw-h-6 tw-w-6" />
-                  Compare
-                </li>
-              </Link>
-              <Link href={`/single-product/${item.attributes.slug}`}>
-                <li className="cursor-pointer  tw-flex tw-items-center tw-gap-1 tw-text-sm">
-                  <Details />
-                  Detail
-                </li>
-              </Link>
-            </ul>
-          </div>
+        //         <span>Like</span>
+        //       </li>
+        //       <Link
+        //         href={`/product-comparison?current=${item.attributes.slug}`}
+        //       >
+        //         <li className="cursor-pointer tw-flex tw-items-center tw-gap-1 tw-text-sm">
+        //           <Compare className="tw-h-6 tw-w-6" />
+        //           Compare
+        //         </li>
+        //       </Link>
+        //       <Link href={`/single-product/${item.attributes.slug}`}>
+        //         <li className="cursor-pointer  tw-flex tw-items-center tw-gap-1 tw-text-sm">
+        //           <Details />
+        //           Detail
+        //         </li>
+        //       </Link>
+        //     </ul>
+        //   </div>
 
-          <Avatar className="tw-absolute tw-right-4 tw-top-4 tw-bg-red-400">
-            <span className="tw-text-sm">{item?.attributes?.disscount}%</span>
-          </Avatar>
+        //   <Avatar className="tw-absolute tw-right-4 tw-top-4 tw-bg-red-400">
+        //     <span className="tw-text-sm">{item?.attributes?.disscount}%</span>
+        //   </Avatar>
 
-          <Image
-            src={
-              process.env.NEXT_PUBLIC_MEDIA +
-              item.attributes.image?.data[0].attributes.url
-            }
-            alt="products"
-            fill={false}
-            width={200}
-            height={200}
-            className="tw-h-72 tw-w-full tw-object-cover"
-          />
-          <Box className="tw-flex tw-flex-col tw-gap-2 tw-p-4">
-            <h3 className="tw-text-xl tw-font-bold">{item.attributes.name}</h3>
-            <p className="tw-text-gray-500">
-              {item.attributes.category?.data?.attributes.category || "-"}
-            </p>
-            <div className="tw-flex tw-flex-col tw-items-start tw-justify-between tw-gap-2 md:tw-flex-row md:tw-items-center">
-              <s className="tw-inline tw-text-sm tw-text-gray-400">
-                {convertCurrency(item?.attributes.price)}
-              </s>
-              <strong className="tw-inline tw-text-default-200">
-                {convertCurrency(
-                  (item?.attributes.disscount / 100) * item?.attributes.price,
-                )}
-              </strong>
-            </div>
-          </Box>
-        </Box>
+        //   <Image
+        //     src={
+        //       process.env.NEXT_PUBLIC_MEDIA +
+        //       item.attributes.image?.data[0].attributes.url
+        //     }
+        //     alt="products"
+        //     fill={false}
+        //     width={200}
+        //     height={200}
+        //     className="tw-h-72 tw-w-full tw-object-cover"
+        //   />
+        //   <Box className="tw-flex tw-flex-col tw-gap-2 tw-p-4">
+        //     <h3 className="tw-text-xl tw-font-bold">{item.attributes.name}</h3>
+        //     <p className="tw-text-gray-500">
+        //       {item.attributes.category?.data?.attributes.category || "-"}
+        //     </p>
+        //     <div className="tw-flex tw-flex-col tw-items-start tw-justify-between tw-gap-2 md:tw-flex-row md:tw-items-center">
+        //       <s className="tw-inline tw-text-sm tw-text-gray-400">
+        //         {convertCurrency(item?.attributes.price)}
+        //       </s>
+        //       <strong className="tw-inline tw-text-default-200">
+        //         {convertCurrency(
+        //           (item?.attributes.disscount / 100) * item?.attributes.price,
+        //         )}
+        //       </strong>
+        //     </div>
+        //   </Box>
+        // </Box>
       ))}
     </div>
   );
