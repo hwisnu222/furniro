075157ebfbd0bcc48791d7a4747548b4c8ad3bdc
Wisnu Box <hwisnu222@gmail.com>
2023-08-24 12:10:40 +0700

    fix: rupiah currency

--- utils/currency.ts
+++ utils/currency.ts
@@ -8,6 +8,7 @@ export const convertCurrency = (number: number) => {
   const c = new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
+    minimumFractionDigits: 0,
   });
   return c.format(number);
 };
